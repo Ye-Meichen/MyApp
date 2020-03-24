8a69a9070dc663f9cc52d4fc40e2ea6767c3cce4
Ye Meichen <3321758734@qq.com>
2020-03-24 11:41:23 +0800

    第四周课堂示例

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -10,22 +10,44 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView out;
+    EditText inp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = this.<TextView>findViewById(R.id.showText);
-        tv.setText("swufe");
-        EditText inp = this.<EditText>findViewById(R.id.inpText);
-        String str = inp.getText().toString();
-        System.out.print("input="+str);
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(this);
+        out = findViewById(R.id.showText);
+        inp = findViewById(R.id.inpText);
+
+
+        Button btn = findViewById(R.id.btn1);
+       // btn.setOnClickListener(this);
+        //btn.setOnClickListener(new View.OnClickListener() {
+        //            @Override
+        //            public void onClick(View v) {
+        //                Log.i("mail","oncliCk called");
+        //                String str = inp.getText().toString();
+        //                out.setText("Hello"+str);
+        //            }
+        //        });
+         //   }
+       // });
     }
 
     @Override
     public void onClick(View v) {
-        Log.i("click","onClick");
+           Log.i("click","onClick");
+    //
+    //        //TextView tv = findViewById(R.id.showText);
+    //
+    //        //EditText inp = findViewById(R.id.inpText);
+          String str = inp.getText().toString();
+
+          out.setText("Hello"+str);
     }
+    public void btnClick(View btn) {
+        Log.i("click", "onClick");
+    }
+
 }
